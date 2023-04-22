404124343acaf8641bd730075980f39cf3c41862
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-23 02:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
-  
+  let rows = prompt("Input number of rows");
+	let cols = prompt("Input number of columns");
+	// console.log(row,col);
+	// console.log(col);
+	let table = document.getElementById("myTable");
+	
+	for(let i = 0; i < rows; i++){
+		let row = document.createElement("tr");
+		for(let j = 0; j < cols; j++){
+			let col = document.createElement("td");
+			col.innerText = `Row-${i} Column-${j}`;
+			row.appendChild(col);
+		}
+		table.appendChild(row);
+	}
+	
 }
